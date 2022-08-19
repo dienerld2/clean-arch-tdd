28b4a4d236f0f36fe0b8aafcefe5d4a2fd13d3ff
Diener Dornelas <dienezim@hotmail.com>
2022-08-19 10:29:27 -0300

    create custom configs to unit and integration tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['**/*.{test|spec}.ts']
 
-};
+}
