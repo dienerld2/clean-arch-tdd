f518c1972d23e08bd58460ea1fc05d51d2e8e00d
Diener Dornelas <dienezim@hotmail.com>
2022-09-06 19:50:09 -0300

    move AuthUsecase to its own file

--- src/domain/use-cases/auth.usecase.spec.ts
+++ src/domain/use-cases/auth.usecase.spec.ts
@@ -1,29 +1,5 @@
 import { MissingParamError } from '../../utils/errors';
-
-class AuthUseCase {
-  constructor (readonly loadUserByEmailRepository: any) {}
-
-  async auth (email: string, password: string) {
-    if (!email) {
-      throw new MissingParamError('email');
-    }
-    if (!password) {
-      throw new MissingParamError('password');
-    }
-    if (!this.loadUserByEmailRepository) {
-      throw new MissingParamError('loadUserByEmailRepository');
-    }
-
-    if (!this.loadUserByEmailRepository.load) {
-      throw new MissingParamError('loadUserByEmailRepository');
-    }
-
-    const user = await this.loadUserByEmailRepository.load(email);
-    if (!user) {
-      return null;
-    }
-  }
-}
+import { AuthUseCase } from './auth.usecase';
 
 const makeSut = () => {
   class LoadUserByEmailRepositorySpy {
